2ea569cb5d21e31eae3996dd29ad8af9e4829025
Shabadoo9 <brinstonzbpe@gmail.com>
2023-10-11 13:31:19 -0400

    started html and added questions to js

---
+++ assets/index.js
@@ -0,0 +1,41 @@
+const question = document.getElementById("question");
+
+let questions = [
+    {
+        question : "What is the result of the following expression: 5 + '5'?",
+        choiceA : "55",
+        choiceB : "10",
+        choiceC : "5 + '5'",
+        correct : "A"
+    },{
+        question : "Which of the following is a valid way to comment in JavaScript?",
+        choiceA : "// This is a comment",
+        choiceB : "<!-- This is a comment -->",
+        choiceC : "/* This is a comment */",
+        correct : "A"
+    },{
+        question : "What is the JavaScript operator for equality comparison?",
+        choiceA : "==",
+        choiceB : "===",
+        choiceC : "=>",
+        correct : "B"
+    },{
+        question : "Which method is used to remove the last item from an array in JavaScript?",
+        choiceA : "pop()",
+        choiceB : "push()",
+        choiceC : "remove()",
+        correct : "A"
+    },{
+        question : "What is the purpose of the `return` statement in a JavaScript function?",
+        choiceA : "To declare a variable",
+        choiceB : "To exit the function and specify a return value",
+        choiceC : "To add a comment",
+        correct : "B"
+    },{
+        question : "In JavaScript, what is the purpose of the `for` loop?",
+        choiceA : "To define a function",
+        choiceB : "To iterate over a block of code a specific number of times",
+        choiceC : "To display a message",
+        correct : "B"
+    }
+];
